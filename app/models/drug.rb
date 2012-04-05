6f2335b39a858fdc50516dfd59ccc64b6a31f254
Jefferson Fernandes <jeffs.fernandes@gmail.com>
2012-04-05 02:22:43 -0300

    Now on edit/create examinations it's possible to select the drugs used/resistance

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -1,4 +1,7 @@
 class Drug < ActiveRecord::Base
 	has_many :drug_uses
 	has_many :drug_resistances
+	
+  has_many :examinationsThatUseTheDrug, :through => :drug_uses, :source => 'examination'
+  has_many :examinationsThatResistToTheDrug, :through => :drug_resistances, :source => 'examination'
 end
--- app/models/examination.rb
+++ app/models/examination.rb
@@ -1,10 +1,15 @@
 class Examination < ActiveRecord::Base
 	belongs_to :patient
 	belongs_to :doctor
+	belongs_to :genotype
+  
 	has_many :drug_uses, :dependent => :destroy
 	has_many :drug_resistances, :dependent => :destroy
+	
 	has_and_belongs_to_many :mutations
-	belongs_to :genotype
+	
+	has_many :usesOfDrugs, :through => :drug_uses, :source => 'drug'
+	has_many :resistancesToDrugs, :through => :drug_resistances, :source => 'drug'
 	
 	def use?(drugUse)
 	   drugs = []
