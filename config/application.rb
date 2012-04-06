f67c257d369e5e38d4268bb9adaa879f597634f0
Jefferson Fernandes <jeffs.fernandes@gmail.com>
2012-04-06 00:49:06 -0300

    changes on database.yml

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ if defined?(Bundler)
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
-
+require "devise"
 module Exames
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
