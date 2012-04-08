fe528e8cc25411368df265b1fa9a79e021e817fb
Jefferson Fernandes <jeffs.fernandes@gmail.com>
2012-04-08 11:12:48 -0300

    config.assets.compile = true

--- config/application.rb
+++ config/application.rb
@@ -45,6 +45,9 @@ module Exames
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'        
                        
-    config.assets.initialize_on_precompile = false   
+    config.assets.initialize_on_precompile = false
+    
+    config.assets.compile = true
+       
   end
 end
