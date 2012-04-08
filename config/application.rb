e175ec1fac41beba496b4d61efcf9e92a81bbb86
Jefferson Fernandes <jeffs.fernandes@gmail.com>
2012-04-08 14:14:18 -0300

    config.serve_static_assets = true

--- config/application.rb
+++ config/application.rb
@@ -47,7 +47,7 @@ module Exames
     
     config.assets.compile = true
                                                       
-    config.assets.precompile = ['featured_slide.css', 'aplication.css']    
+    config.assets.precompile = ['featured_slide.css', 'application.css']    
     
     config.serve_static_assets = true
                                                                                      
