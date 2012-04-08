54d352e2c4826f38a7ff722c8eb3500427e16f12
Jefferson Fernandes <jeffs.fernandes@gmail.com>
2012-04-08 11:08:09 -0300

    bundle exec rake assets:precompile runned locally

--- config/application.rb
+++ config/application.rb
@@ -44,8 +44,7 @@ module Exames
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'        
-    
-    config.assets.precompile = true
-    
+                       
+    config.assets.initialize_on_precompile = false   
   end
 end
